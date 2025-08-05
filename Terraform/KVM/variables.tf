ce2438eb839e8e3647a3bb3b2cb588f065331851
Artem Shestakov <artem.s.shestakov@gmail.com>
2025-08-06 01:24:02 +0300

    Add Terraform KVM example

---
+++ Terraform/KVM/variables.tf
@@ -0,0 +1,24 @@
+variable "libvirt_disk_path" {
+  description = "path for libvirt pool"
+  default     = "/opt/kvm/pool1"
+}
+
+variable "ubuntu_18_img_url" {
+  description = "ubuntu 24.04 image"
+  default     = "https://cloud-images.ubuntu.com/releases/noble/release/ubuntu-24.04-server-cloudimg-amd64.img"
+}
+
+variable "vm_hostname" {
+  description = "vm hostname"
+  default     = "terraform-kvm-ansible"
+}
+
+variable "ssh_username" {
+  description = "the ssh user to use"
+  default     = "ubuntu"
+}
+
+variable "ssh_private_key" {
+  description = "the private key to use"
+  default     = "~/.ssh/id_ed25519"
+}
